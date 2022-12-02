66f35f09cee73c7cc4092c1bbbcedc86b92f1a41
atipozitiv <117564743+atipozitiv@users.noreply.github.com>
2022-12-02 23:42:33 +0700

    Update zmeya.cpp

--- zmeya.cpp
+++ zmeya.cpp
@@ -8,7 +8,14 @@
 #include <iostream>
 #include <windows.h>
 #include <locale.h>
+#include <string>
 using namespace std;
+
+int snakeHead[2] = {2, 7};
+int snakeLength = 3;
+int score = 0;
+bool alive = true;
+char orientation = 's';
 
 void firstMiniature() {
   cout << " Zmeyka    ih     k     lqsdfh\n";
@@ -21,85 +28,141 @@ void firstMiniature() {
   Sleep(500);
   cout << "  o      i     y     q  s\n";
   cout << " Zmeyka  m     v     s  arwrtf\n\n";
-  Sleep(2000);
+  Sleep(1000);
   cout << " Y     E   K   W         A\n";
+  Sleep(300);
   cout << "  L   Q    E  Q         D Q\n";
+  Sleep(50);
   cout << "   E S     ZDB         F   E\n";
+  Sleep(300);
   cout << "    D      X  F       QDSQKE\n";
+  Sleep(300);
   cout << "   S       Q   W     E      Q\n";
-  cout << "  K        D    A   R        S\n";
+  Sleep(100);
+  cout << "  K        D    A   R        S\n\n";
   Sleep(2000);
 }
 
 void secondMiniature() {
-  cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
-  cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
-  cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
-  cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
-  cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
-  cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
-  cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
-}
+  while (true) {
+    cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
+    cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
+    cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
+    cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
+    cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
+    cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
+    cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
+    cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
+    cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
+    cout << "ТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\nТЫПРОИГРАЛТЫПРОИГРАЛТЫПРОИГРАЛ\n";
+    system("cls");
+  }
+} 
 
-void playGame() {
-  cout << ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n";
-  cout << "|                             |\n";
-  cout << "|              *              |\n";
-  cout << "|              *              |\n";
-  cout << "|              0              |\n";
-  cout << "|                             |\n";
-  cout << "|                             |\n";
-  cout << "|                             |\n";
-  cout << "|                             |\n";
-  cout << "|                             |\n";
-  cout << "|                             |\n";
-  cout << "|                             |\n";
-  cout << "'''''''''''''''''''''''''''''''";
-}        
+/*                ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+ карта:           |                             |
+                  |                             |
+                  |                             |
+                  |                             |
+                  |                             |
+                  |                             |
+                  |                             |
+                  |                             |
+                  |                             |
+                  |                             |
+                  |                             |
+                  |                             |
+                  |                             |
+                  '''''''''''''''''''''''''''''''
 
-void moveHead() {
+                  счет: 
+*/
+
+// 0 - ничего ; 1 - тело ; 2 - голова
+int snakeOnMap[13][15] = { {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} };
+
+void moveSnake() {
   if (orientation == 's') {
-
+    if ((snakeHead[0] == 12) || (snakeOnMap[snakeHead[0] + 1][snakeHead[1]] == 1)) alive = false;
+    snakeHead[0] += 1;
+    snakeOnMap[snakeHead[0]][snakeHead[1]] = 2;
+    snakeOnMap[snakeHead[0] - 1][snakeHead[1]] = 1;
+  }
+  if (orientation == 'w') {
+    if ((snakeHead[0] == 0) || (snakeOnMap[snakeHead[0] - 1][snakeHead[1]] == 1)) alive = false;
+    snakeHead[0] -= 1;
+    snakeOnMap[snakeHead[0]][snakeHead[1]] = 2;
+    snakeOnMap[snakeHead[0] + 1][snakeHead[1]] = 1;
+  }
+  if (orientation == 'a') {
+    if ((snakeHead[1] == 0) || (snakeOnMap[snakeHead[0]][snakeHead[1] - 1] == 1)) alive = false;
+    snakeHead[1] -= 1;
+    snakeOnMap[snakeHead[0]][snakeHead[1]] = 2;
+    snakeOnMap[snakeHead[0]][snakeHead[1] + 1] = 1;
+  }
+  if (orientation == 'd') {
+    if ((snakeHead[1] == 14) || (snakeOnMap[snakeHead[0]][snakeHead[1] + 1] == 1)) alive = false;
+    snakeHead[1] += 1;
+    snakeOnMap[snakeHead[0]][snakeHead[1]] = 2;
+    snakeOnMap[snakeHead[0]][snakeHead[1] - 1] = 1;
   }
 }
 
-void deleteTail() {
-  //удалить хвост(можно будет 
+char checkPosition(int string, int column) {
+  if (snakeOnMap[string][column] == 0) return ' ';
+  if (snakeOnMap[string][column] == 1) return '*';
+  return '0';
 }
 
-void spawnApple() {
-  //удалить старое яблоко и создать новое
-}
+void playGame() {
+  moveSnake();
+  system("cls");
+  string gameWindow = ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n";
+  for (int snakeString = 0; snakeString < 13; ++snakeString) {
+    gameWindow += "|";
+    for (int snakeColumn = 0; snakeColumn < 14; ++snakeColumn) {
+      gameWindow = gameWindow + checkPosition(snakeString, snakeColumn) + " ";
+    }
+    gameWindow = gameWindow + checkPosition(snakeString, 14) + "|\n";
+  }
+  gameWindow = gameWindow + "'''''''''''''''''''''''''''''''\n\nсчет: " + to_string(score);
+  cout << gameWindow;
 
-int apple[2];
-bool alive = true;
-int snakesHead[2] = {8, 4};
-int sneakesTail[2] = {0, 0};
-char orientation = 's';
+}
 
 int main() {
-  system("mode con cols=31 lines=20");
+  system("mode con cols=31 lines=17");
   setlocale(LC_ALL, "Russian");
   firstMiniature();
   system("pause");
   system("cls");
   playGame();
+  Sleep(350);
 
+  int speed = 0;
   while (alive) {
     if (GetAsyncKeyState(0x57)) orientation = 'w';
     if (GetAsyncKeyState(0x41)) orientation = 'a';
     if (GetAsyncKeyState(0x53)) orientation = 's';
     if (GetAsyncKeyState(0x44)) orientation = 'd';
-    moveHead();
-    if ((snakesHead[0] == apple[0]) && (snakesHead[1] == apple[1])) {
-      spawnApple();
-    } else {
-      deleteTail();
-    }
+    if (speed == 49999) playGame();
+    ++speed;
+    speed = speed % 50000;
   }
 
   system("cls");
   secondMiniature();
-  Sleep(3000);
   return 0;
 }
